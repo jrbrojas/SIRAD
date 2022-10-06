4a65c5a9884fdcb45b2a6492d1cc1410858d3393
unknown <luis.hernandez.albia@gmail.com>
2022-10-06 17:34:01 -0500

    update mantemiento categoria y subcategoria

--- src/app/pages/sirad/registro/edit-subcategories-modal/edit-subcategories-modal.component.ts
+++ src/app/pages/sirad/registro/edit-subcategories-modal/edit-subcategories-modal.component.ts
@@ -1,5 +1,6 @@
 import { ENTER, COMMA } from '@angular/cdk/keycodes';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import Swal from 'sweetalert2';
@@ -61,8 +62,12 @@ export class FichaEvaluacion {
   styleUrls: ['./edit-subcategories-modal.component.scss']
 })
 export class EditSubcategoriesModalComponent implements OnInit {
+  public dataSubCategories : Array<any> = []
+  public updateFormSubCategories : FormGroup;
 
   formFichaEvaluacion: FichaEvaluacion = new FichaEvaluacion();
+
+  
 
 
   dataTipoRespuesta = [
@@ -89,11 +94,93 @@ export class EditSubcategoriesModalComponent implements OnInit {
   ];
 
   constructor(
-    private modalService : NgbModal
-  ) { }
+    private modalService : NgbModal,
+    private fb : FormBuilder
+  ) { 
+
+    this.updateFormSubCategories = this.fb.group({
+      codCategory : ['', [Validators.required]],
+      codSubCategory : ['', [Validators.required]],
+      nameSubCategory : ['', [Validators.required]],
+      keyWords : ['' ,[Validators.required]],
+      keyWordsGeo : ['', [Validators.required]],
+      summary : ['', [Validators.required]],
+      infoExtended : ['', [Validators.required]],
+      infoDate : ['', [Validators.required]],
+      updateDateFinal : ['', [Validators.required]],
+      dataSource : ['', [Validators.required]],
+      generalManager : ['', [Validators.required]],
+      reponsibleInfoLayer : ['', [Validators.required]],
+      contactDataSource : ['', [Validators.required]],
+      typeGeo : ['', [Validators.required]],
+      systemCoord : ['', [Validators.required]]
+    })
+
+  }
 
   ngOnInit(): void {
+    this.dataSubCategories.push({
+      codCategory : 'C0001',
+      codSubCategory : 'SB0001',
+      nameSubCategory : 'Plantas de Agua Potable',
+      keyWords : ['Agua Potable'],
+      keyWordsGeo : 'Lima/Callao',
+      summary : `Descripción básica de las plantas de producción de agua potable en términos de producción y de tipo de instalaciones, reservorios etc…`,
+      infoExtended : `Proyecto “Elaboración de un Sistema de Información Geográfico y Análisis de Recursos Esenciales para la Respuesta y Recuperación Temprana ante la Ocurrencia de un sismo y/o Tsunami en el Área Metropolitana de Lima y Callao” (Proyecto SIRAD Convocatoria PNUD/SDP-052/2009 / 22 de abril - 15 febrero 2011).
+      Practicas de Jérémy Guilbaud, estudiante ingeniero de la Escuela Nacional de Ciencias Geográficas de Francia.
+      `,
+      infoDate : '2010',
+      updateDateFinal : '2010-07-26',
+      dataSource : 'Sedapal',
+      generalManager : `Roberto D'Ercole (IRD)`,
+      responsibleInfoLayer : 'Pascale Mertzger(IRD)',
+      contactDataSource: ['Pierre Vernier', 'Pauline Gluski', 'Jeremy Guilbaud'],
+      typeGeo : 'Punto',
+      systemCoord: 'EPSG: 32718 - WGS 84 UTM zona 18 sur'
+
+    },
+    {
+      codCategory : 'C0001',
+      codSubCategory : 'SB0002',
+      nameSubCategory : 'Pozos',
+      keyWords : ['Agua Potable', 'Pozos'],
+      keyWordsGeo : 'Lima/Callao',
+      summary : `Ubicación y descripción de los pozos administrados por SEDAPAL con elementos técnicos y operacionales relativos a pozos y bombas`,
+      infoExtended : `Proyecto “Elaboración de un Sistema de Información Geográfico y Análisis de Recursos Esenciales para la Respuesta y Recuperación Temprana ante la Ocurrencia de un sismo y/o Tsunami en el Área Metropolitana de Lima y Callao” (Proyecto SIRAD Convocatoria PNUD/SDP-052/2009 / 22 de abril - 15 febrero 2011).
+      Practicas de Jérémy Guilbaud, estudiante ingeniero de la Escuela Nacional de Ciencias Geográficas de Francia.
+      .
+      `,
+      infoDate : '2010',
+      updateDateFinal : '2010-10-08',
+      dataSource : 'Sedapal',
+      generalManager : `Roberto D'Ercole (IRD)`,
+      responsibleInfoLayer : 'Pascale Mertzger(IRD)',
+      contactDataSource: ['Pierre Vernier', 'Pauline Gluski', 'Jeremy Guilbaud'],
+      typeGeo : 'Punto',
+      systemCoord: 'EPSG: 32718 - WGS 84 UTM zona 18 sur'
+    })
+
+    this.getDetailsSubCategory();
+
+
   }
+
+  getDetailsSubCategory(){
+    this.updateFormSubCategories.controls['nameSubCategory'].setValue(this.dataSubCategories[0].nameSubCategory)
+    this.updateFormSubCategories.controls['keyWords'].setValue(this.dataSubCategories[0].keyWords)
+    this.updateFormSubCategories.controls['keyWordsGeo'].setValue(this.dataSubCategories[0].keyWordsGeo)
+    this.updateFormSubCategories.controls['summary'].setValue(this.dataSubCategories[0].summary)
+    this.updateFormSubCategories.controls['infoExtended'].setValue(this.dataSubCategories[0].infoExtended)
+    this.updateFormSubCategories.controls['infoDate'].setValue(this.dataSubCategories[0].infoDate)
+    this.updateFormSubCategories.controls['updateDateFinal'].setValue(this.dataSubCategories[0].updateDateFinal)
+    this.updateFormSubCategories.controls['dataSource'].setValue(this.dataSubCategories[0].dataSource)
+    this.updateFormSubCategories.controls['generalManager'].setValue(this.dataSubCategories[0].generalManager)
+    this.updateFormSubCategories.controls['responsibleInfoLayer'].setValue(this.dataSubCategories[0].responsibleInfoLayer)
+    this.updateFormSubCategories.controls['contactDataSource'].setValue(this.dataSubCategories[0].contactDataSource)
+    this.updateFormSubCategories.controls['typeGeo'].setValue(this.dataSubCategories[0].typeGeo)
+    this.updateFormSubCategories.controls['systemCoord'].setValue(this.dataSubCategories[0].systemCoord)
+  }
+
 
   closeModal(){
     this.modalService.dismissAll()
